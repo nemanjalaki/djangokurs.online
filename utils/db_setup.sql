4f8835de49badb6ed341c6ede429fdf179c7d8cd
[nemanja lakicevic] <[sone.lakicevic@gmail.com]>
2023-08-29 10:11:36 +0200

    ignore utils dir

--- utils/db_setup.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE djangokurs;
-CREATE USER djangokurs_adminuser WITH PASSWORD 'Sifra123!';
-ALTER ROLE djangokurs_adminuser SET client_encoding TO 'utf8';
-ALTER ROLE djangokurs_adminuser SET default_transaction_isolation TO 'read committed';
-ALTER ROLE djangokurs_adminuser SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE djangokurs TO djangokurs_adminuser;
-
-ALTER USER djangokurs_adminuser WITH PASSWORD 'Sifra123!';
